f7a2b0806b64b76d46ff321c4a752738208e81c3
Callum Kloos <callumkloos@gmail.com>
2023-10-14 13:48:14 +0100

    installed

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,15 +2,15 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "The Wall" },
+    { name: "description", content: "This is Your Wall" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="m-4 w-full bg-red-50 p-10 text-center">
-      <h1 className="text-4xl">Hello World</h1>
+    <div className="m-4 w-full bg-primary p-10 text-center">
+      <h1 className="text-4xl">Hello, Deployment</h1>
     </div>
   );
 }
--- app/routes/feed.tsx
+++ app/routes/feed.tsx
@@ -1,15 +1,38 @@
 import { Link, Outlet } from "@remix-run/react";
+import {
+  Button,
+  Item,
+  Label,
+  ListBox,
+  Popover,
+  Select,
+  SelectValue,
+} from "react-aria-components";
 import { useState } from "react";
 
 export default function FeedPage() {
   const [count, setCount] = useState<number>(0);
   return (
     <div className="mx-auto flex w-4/5 flex-col gap-5 p-10">
-      <h1>This is the feed page</h1>
+      <h1 className="text-primary">This is the feed page</h1>
       <Links />
       <p>{count}</p>
       <button onClick={() => setCount(count + 1)}>add one</button>
       <Outlet />
+      <Select>
+        <Label>Favorite Animal</Label>
+        <Button>
+          <SelectValue />
+          <span aria-hidden="true">▼</span>
+        </Button>
+        <Popover>
+          <ListBox>
+            <Item>Cat</Item>
+            <Item>Dog</Item>
+            <Item>Kangaroo</Item>
+          </ListBox>
+        </Popover>
+      </Select>
     </div>
   );
 }
